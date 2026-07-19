1d44cbbea866f74976b856fd425d1aff9d8322f8
Rakchi <ZNAC416@live.rhul.ac.uk>
2026-07-19 22:17:43 +0100

    set up inital flask structure

--- flask_budget.py
+++ flask_budget.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask , render_template
 
 app = Flask(__name__) 
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return "Hello BudgetAi"
+    return render_template('index.html')
 
 @app.route("/about")
 def about():
